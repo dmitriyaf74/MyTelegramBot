0c362f4d52f5c4fa6c44a21132d15b84a61077e5
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-02-16 10:23:39 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace HomeWork24
+{
+    internal class Program
+    {
+        private static void UpdateReceived(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            //var msg:string = update.Message?.ToString() ?? "Error";
+            //var msg:string? = update.Message != null ? update.Message.ToString() ?? "Error";
+            if (update?.Message?.From is null)
+            {
+                Console.WriteLine("Message is null");
+                return;
+            }
+
+            var info = $"{update.Message.From.Username} : {update.Message.Text}";
+            Console.WriteLine(info);
+            botClient.SendMessage(update.Message.Chat.Id, info, cancellationToken: token);
+        }
+
+        static async Task Main()
+        {
+            var cts = new CancellationTokenSource();
+            var bot = new TelegramBotClient("");
+
+            var me = await bot.GetMe(); //Информация о боте
+            Console.WriteLine($"Hello, {me.FirstName} {me.LastName} with {me.Username} an id {me.Id}!");
+            bot.StartReceiving(UpdateReceived, (_, exception, _) => Console.WriteLine(exception.Message), new Telegram.Bot.Polling.ReceiverOptions()
+            {
+                AllowedUpdates = [Telegram.Bot.Types.Enums.UpdateType.Message]
+            }, cts.Token);
+            Console.ReadLine();
+            await cts.CancelAsync();
+        }
+
+    }
+}
